a10c931734acf62e83910a7bc25e5ce8a35457b8
cgray1117 <cag5911@psu.edu>
2023-11-27 10:55:53 -0500

    initial button class

---
+++ python-checkers-main/button.py
@@ -0,0 +1,28 @@
+import pygame
+import sys
+
+main_font = pygame.font.SysFont("cambria", 50)
+
+class Button():
+    def __init__(self, image, x_pos, y_pos, text_input):
+        self.image = image
+        self.x_pos = x_pos
+        self.y_pos = y_pos
+        self.rect = self.image.get_rect(center=(self.x_pos, self.y_pos))
+        self.text_input = text_input
+        self.text = main_font.render(self.text_input, True, "white")
+        self.text_rect = self.text.get_rect(center=(self.x_pos, self.y_pos))
+
+    def update(self):
+        screen.blit(self.image, self.rect)
+        screen.blit (self.text, self.text_rect)
+
+    def checkForInput(self, position):
+        if position[0] in range(self.rect.left, self.rect.right) and position[1] in range(self.rect.top, self.rect.bottom):
+            pass
+
+    def chnageColor(self, position):
+        if position[0] in range(self.rect.left, self.rect.right) and position[1] in range(self.rect.top, self.rect.bottom):
+            pass
+        else:
+            pass
