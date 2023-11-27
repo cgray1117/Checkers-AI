7c84ce7f9f9be73d7ba07d0a450796354b49c1c6
cgray1117 <cag5911@psu.edu>
2023-11-27 12:57:04 -0500

    added alpha beta pruning to algorithm

--- python-checkers-main/minimax/algorithm.py
+++ python-checkers-main/minimax/algorithm.py
@@ -3,30 +3,39 @@ import pygame
 
 RED = (255,0,0)
 WHITE = (255, 255, 255)
+MAX = float('inf')
+MIN = float('-inf')
 
 
 #Defining the MINIMAX algorithm
 #Evaluates best move 
-def minimax(position, depth, max_player, game):
+def minimax(position, depth, max_player, game, ALPHA=MIN, BETA=MAX):
     if depth == 0 or position.winner() != None:
         return position.evaluate(), position
     #Finds the maximum win
     if max_player:
-        maxEval = float('-inf')
+        maxEval = MIN
         best_move = None
         for move in get_all_moves(position, WHITE, game):
-            evaluation = minimax(move, depth-1, False, game)[0]
+            evaluation = minimax(move, depth-1, False, game, ALPHA, BETA)[0]
             maxEval = max(maxEval, evaluation)
+            ALPHA = max(ALPHA, maxEval)
+            if BETA <= ALPHA:
+                break
             if maxEval == evaluation:
                 best_move = move
         
         return maxEval, best_move
+    
     else: #evaluates the move that brings the minimum win for the opponent
-        minEval = float('inf')
+        minEval = MAX
         best_move = None
         for move in get_all_moves(position, RED, game):
-            evaluation = minimax(move, depth-1, True, game)[0]
+            evaluation = minimax(move, depth-1, True, game, ALPHA, BETA)[0]
             minEval = min(minEval, evaluation)
+            BETA = min(BETA, minEval)
+            if BETA <= ALPHA:
+                break
             if minEval == evaluation:
                 best_move = move
         
